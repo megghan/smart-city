0b178123b812d6acfef5dd7ca399f58b1b4fc47d
Megghan Agnes <agnesmegghan@gmail.com>
2025-10-06 18:49:03 -0300

    resolvendo conflito versoes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "@/components/AuthProvider";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
+// Metadata do site
 export const metadata: Metadata = {
   title: "ConectaPlus",
   description: "Para Cidades inteligentes e inclusivas.",
@@ -25,13 +15,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      {/* Aqui usamos classes CSS normais para a fonte */}
+      <body className="font-sans antialiased">
         <AuthProvider>
           {children}
         </AuthProvider>
-        
       </body>
     </html>
   );
